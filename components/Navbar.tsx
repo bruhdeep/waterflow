a14ce15ec727fffede2d6a3afc83606ee4709290
Pradip Shrestha <shresthapradeep47@gmail.com>
2023-12-12 17:46:24 +0545

    initial commit
    created the workspace and made some minor changes

---
+++ components/Navbar.tsx
@@ -0,0 +1,21 @@
+'use client'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="main">
+			<div className="logo">
+				Hola
+			</div>
+			<ul>
+				<li className="active"><a href="#">Home</a></li>
+				<li><a href="https://blackclover.fandom.com/wiki/Black_Clover_Wiki">Fandom</a></li>
+				<li><a href="Gallery.html">Gallery</a></li>
+				<li><a href="About.html">About</a></li>
+				<li><a href="Contact.html">Contact</a></li>
+			</ul>
+		</div>
+  )
+}
+
+export default Navbar
